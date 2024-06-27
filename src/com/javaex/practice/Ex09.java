914db4fdfd62684c01f77dc47a7855347679fcc2
jiny0852 <jiny0852@gmail.com>
2024-06-27 18:06:04 +0900

    2024-06-27 별별무슨별

--- src/com/javaex/practice/Ex09.java
+++ src/com/javaex/practice/Ex09.java
@@ -10,7 +10,7 @@ public class Ex09 {
 		for(int i = 1; i < 11 ; i++) {
 			
 			for (int j = 0 ; j < 10 ; j++) {
-				System.out.print(i+j);
+				System.out.print(i+j + "  ");
 			}
 			
 			System.out.println();
